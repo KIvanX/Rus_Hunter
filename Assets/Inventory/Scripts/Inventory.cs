e7e8acbcfcea3a6d10db59a18388fed8c0d04751
RuspeLoke <ruslan.khamzin712@gmail.com>
2023-04-23 22:27:28 +0300

    [add] Short inventory

---
+++ Assets/Inventory/Scripts/Inventory.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class InventorySlot
+{
+    public Item Item;
+    public int Amount;
+
+    public InventorySlot(Item item, int amount = 1)
+    {
+        Item = item;
+        Amount = amount;
+    }
+}
+
+public class Inventory : MonoBehaviour
+{
+    [SerializeField] private List<InventorySlot> _slots = new();
+
+    public UnityEvent OnInventoryUpdate;
+
+    public void AddItems(Item item, int amount = 1)
+    {
+        foreach (InventorySlot slot in _slots)
+        {
+            if (slot.Item.Id == item.Id)
+            {
+                slot.Amount += amount;
+                OnInventoryUpdate.Invoke();
+                return;
+            }
+        }
+
+        _slots.Add(new InventorySlot(item, amount));
+        OnInventoryUpdate.Invoke();
+    }
+
+    public int GetSize()
+    {
+        return _slots.Count;
+    }
+
+    public InventorySlot this[int index]
+    {
+        get => _slots[index];
+    }
+}
---
+++ Assets/Inventory/Scripts/ShortInventoryUI.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShortInventoryUI : MonoBehaviour
+{
+    [SerializeField] private List<Image> _icons = new();
+    [SerializeField] private List<TextMeshProUGUI> _amounts = new();
+
+    public void UpdateUI(Inventory inventory)
+    {
+        for (int i = 0; i < _icons.Count; i++)
+        {
+            if (i < inventory.GetSize())
+            {
+                _icons[i].color = new Color(1, 1, 1, 1);
+                _icons[i].sprite = inventory[i].Item.Icon;
+                _amounts[i].text = inventory[i].Amount > 1 ? inventory[i].Amount.ToString() : "";
+            }
+            else
+            {
+                _icons[i].color = new Color(1, 1, 1, 0);
+                _amounts[i].text = "";
+            }
+        }
+    }
+}
